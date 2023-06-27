ff8ffdd44dc16305fd78dd874b41f8b5092383eb
422UR4H <tiagosindra@id.uff.br>
2023-06-27 01:09:35 -0300

    basic server draft

--- src/app.js
+++ src/app.js
@@ -9,10 +9,57 @@ app.use(express());
 app.use(cors());
 app.use(json());
 
-app.get("/test", (req, res) => {
+
+app.post("/participants", (req, res) => {
+    const { name } = req.body;
+
+    // validations
+
+    // save in mongo
+
+    res.sendStatus(201);
+});
+
+app.get("/participants", (req, res) => {
     console.log("test");
 });
 
+app.post("/messages", (req, res) => {
+    const { to, text, type } = req.body;
+    const from = req.headers.User;
+
+    // validations
+
+    // save in mongo
+
+    res.sendStatus(201);
+});
+
+app.get("/messages", (req, res) => {
+    const limit = parseInt(req.query.limit);
+    if (!limit || limit < 1) return res.sendStatus(422);
+    
+    const { User } = req.headers;
+
+    // get messages
+
+    // messages filter logic
+    // limit filter
+    // to, from and "Todos" filter
+
+    // send messages filter data
+});
+
+app.post("/status", (req, res) => {
+    const { User } = req.headers;
+    if (!User) return res.sendStatus(404);
+
+    // User validation => if (!participantsList.some(participant => participant.? === User)) return sendStatus(404);
+
+    // update lastStatus of User using Date.now()
+
+    res.sendStatus(200);
+});
 
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
