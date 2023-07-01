1cd37432c33bcfbc7696ae5918131cf7ccf6c178
422UR4H <tiagosindra@id.uff.br>
2023-07-01 19:59:00 -0300

    data sanitization with stripHTML

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ import express, { json } from "express";
 import cors from "cors";
 import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
-import joi from "joi";
 import dayjs from "dayjs";
+import joi from "joi";
+import { stripHTML } from "string-strip-html";
+// import filterInactiveParticipants from "./filterInactiveParticipants.js";
 
 
 // SETTINGS
@@ -25,27 +27,33 @@ app.use(express());
 app.use(cors());
 app.use(json());
 
+// try {
+//     filterInactiveParticipants();
+// } catch (err) {
+//     console.error("THE FILTER INACTIVE PARTICIPANTS IS NOT STARTED")
+//     console.log(err.message);
+// }
+
 
 // ENDPOINTS
 
 app.post("/participants", async (req, res) => {
-    const { name } = req.body;
-
     const nameSchema = joi.object({
-        name: joi.string().required()
+        name: joi.string().required().stripHTML()
     });
-    const validation = nameSchema.validate(req.body, { abortEarly: false });
+    const { error, value } = nameSchema.validate(req.body, { abortEarly: false });
 
-    if (validation.error) {
-        const errorMessages = validation.error.details.map(detail => detail.message);
+    if (error) {
+        const errorMessages = error.details.map(detail => detail.message);
         return res.status(422).send(errorMessages);
     }
-
-    if (await db.collection("participants").findOne({ name })) {
-        return res.status(409).send("Nome de usuário já utilizado. Tente outro!");
-    }
+    const { name } = value;
 
     try {
+        if (await db.collection("participants").findOne({ name })) {
+            return res.status(409).send("Nome de usuário já utilizado. Tente outro!");
+        }
+
         await db.collection("participants").insertOne({ name, lastStatus: Date.now() });
         console.log(Date.now());
         console.log(dayjs().locale("pt-br"));
@@ -73,24 +81,23 @@ app.get("/participants", async (req, res) => {
 });
 
 app.post("/messages", async (req, res) => {
-    const { to, text, type } = req.body;
     const from = req.headers.user;
-
     if (!(await db.collection("participants").findOne({ name: from }))) {
         return res.status(422).send("Participante não conectado!");
     }
 
     const messageSchema = joi.object({
-        to: joi.string().required(),
-        text: joi.string().required(),
-        type: joi.string().required().allow("message", "private_message")
+        to: joi.string().stripHTML().required(),
+        text: joi.string().stripHTML().required(),
+        type: joi.string().stripHTML().required().allow("message", "private_message")
     });
-    const validation = messageSchema.validate(req.body, { abortEarly: false });
+    const { error, value } = messageSchema.validate(req.body, { abortEarly: false });
 
-    if (validation.error) {
-        const errorMessages = validation.error.details.map(detail => detail.message);
+    if (error) {
+        const errorMessages = error.details.map(detail => detail.message);
         return res.status(422).send(errorMessages);
     }
+    const { to, text, type } = value;
 
     try {
         const time = dayjs().locale("pt-br").format("HH:mm:ss");
@@ -103,33 +110,42 @@ app.post("/messages", async (req, res) => {
 });
 
 app.get("/messages", async (req, res) => {
-    let { limit } = req.query;
+    let limit = stripHTML(req.query.limit);
 
     if (limit) {
         limit = parseInt(limit);
         if (!limit || limit < 1) return res.sendStatus(422);
-    } // talvez colocar um else com um novo valor pra ele
+    }
 
-    const { User } = req.headers;
+    const user = req.headers.user;
+    const messages = await db.collection("messages").find({
+        $or: [
+            { to: { $in: ["Todos", user] } },
+            { type: "message" },
+            { from: user }
+        ]
+    }).toArray();
+    console.log(messages)
 
-    // pesquisar sobre $or do mmongodb
-    const messages = await db.collection("messages").find().toArray()
-        .filter()   // messages filter logic
-        .filter()   // limit filter
-        .filter()   // to, from and "Todos" filter
-        .filter(() => console.log());   // // send messages filter data
-
+    // limit filter with slice (remember to use: -limit || -messages.length)
+    res.send(messages);
 });
 
 app.post("/status", async (req, res) => {
-    const name = req.headers.User;
+    const name = req.headers.user;
     if (!name) return res.sendStatus(404);
 
-    if (!(await db.collection("participants").findOne({ name }))) return res.sendStatus(404);
+    try {
+        const result = await db.collection("participants").updateOne(
+            { name },
+            { $set: { lastStatus: Date.now() } }
+        )
+        if (result.matchedCount === 0) return res.sendStatus(404);
 
-    // update lastStatus of User using Date.now() with db.put
-
-    res.sendStatus(200);
+        res.sendStatus(200);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 });
 
 
---
+++ src/filterInactiveParticipants.js
@@ -0,0 +1,7 @@
+const MS_TIME = 15 * 1000;
+
+export default async function filterInactiveParticipants(participants) {
+    const intervalID = setInterval(() => {
+        // mongoDB filter with query selectors
+    }, MS_TIME);
+}
