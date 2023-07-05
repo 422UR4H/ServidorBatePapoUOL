0810c1a1b9c88bf987b3a42ee0b4fa4ad0ee01ba
422UR4H <tiagosindra@id.uff.br>
2023-07-05 15:39:13 -0300

    code refactored

--- src/app.js
+++ src/app.js
@@ -13,10 +13,8 @@ import filterInactiveParticipants from "./filterInactiveParticipants.js";
 
 const app = express();
 
-app.use(express());
 app.use(cors());
 app.use(json());
-
 dotenv.config();
 
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
@@ -30,13 +28,21 @@ const db = mongoClient.db();
 setInterval(filterInactiveParticipants, MS_INTERVAL, db);
 
 
+// SCHEMAS
+const participantSchema = Joi.object({
+    name: Joi.string().required().custom(value => stripHtml(value)).trim()
+});
+const messageSchema = Joi.object({
+    to: Joi.string().custom(value => stripHtml(value)).trim().required(),
+    text: Joi.string().custom(value => stripHtml(value)).trim().required(),
+    type: Joi.string().custom(value => stripHtml(value)).required().valid("message", "private_message")
+});
+
+
 // ENDPOINTS
 
 app.post("/participants", async (req, res) => {
-    const nameSchema = Joi.object({
-        name: Joi.string().required().custom(value => stripHtml(value)).trim()
-    });
-    const { error, value } = nameSchema.validate(req.body, { abortEarly: false });
+    const { error, value } = participantSchema.validate(req.body, { abortEarly: false });
 
     if (error) {
         const errorMessages = error.details.map(detail => detail.message);
@@ -48,15 +54,16 @@ app.post("/participants", async (req, res) => {
         if (await db.collection("participants").findOne({ name })) {
             return res.status(409).send("Nome de usuário já utilizado. Tente outro!");
         }
+        const timestamp = Date.now();
 
-        await db.collection("participants").insertOne({ name, lastStatus: Date.now() });
+        await db.collection("participants").insertOne({ name, lastStatus: timestamp });
         await db.collection("messages").insertOne(
             {
                 from: name,
                 to: "Todos",
                 text: "entra na sala...",
                 type: "status",
-                time: dayjs().locale("pt-br").format("HH:mm:ss")
+                time: dayjs(timestamp).locale("pt-br").format("HH:mm:ss")
             }
         );
         res.sendStatus(201);
@@ -74,19 +81,14 @@ app.get("/participants", async (req, res) => {
 });
 
 app.post("/messages", async (req, res) => {
-    const messageSchema = Joi.object({
-        to: Joi.string().custom(value => stripHtml(value)).trim().required(),
-        text: Joi.string().custom(value => stripHtml(value)).trim().required(),
-        type: Joi.string().custom(value => stripHtml(value)).required().valid("message", "private_message")
-    });
-    const { error, value } = messageSchema.validate(req.body, { abortEarly: false });
+    const from = req.headers.user;
+    const { error, value } = messageSchema.validate({ ...req.body, from }, { abortEarly: false });
 
     if (error) return res.status(422).send(error.details.map(detail => detail.message));
 
     const to = value.to.result;
     const text = value.text.result;
     const { type } = value;
-    const from = req.headers.user;
 
     try {
         if (!(await db.collection("participants").findOne({ name: from }))) {
@@ -106,19 +108,16 @@ app.get("/messages", async (req, res) => {
     if (limit) {
         limit = parseInt(stripHtml(limit).result);
         if (!limit || limit < 1) return res.sendStatus(422);
-    } else {
-        limit = 0;
     }
-    const user = req.headers.user;
+    const from = req.headers.user;
 
     try {
-        res.send(await db.collection("messages").find({
-            $or: [
-                { to: { $in: ["Todos", user] } },
-                { type: "message" },
-                { from: user }
-            ]
-        }).sort({ _id: -1 }).limit(limit).toArray());
+        res.send(await db
+            .collection("messages")
+            .find({ $or: [{ to: { $in: ["Todos", from] } }, { type: "message" }, { from }] })
+            .sort({ _id: -1 })
+            .limit(limit ? limit : 0)
+            .toArray());
     } catch (err) {
         res.status(500).send(err.message);
     }
@@ -130,8 +129,7 @@ app.post("/status", async (req, res) => {
 
     try {
         const result = await db.collection("participants").updateOne(
-            { name },
-            { $set: { lastStatus: Date.now() } }
+            { name }, { $set: { lastStatus: Date.now() } }
         )
         if (result.matchedCount === 0) return res.sendStatus(404);
 
@@ -151,20 +149,15 @@ app.delete("/messages/:id", async (req, res) => {
         if (name !== message.from) return res.sendStatus(401);
 
         await db.collection("messages").deleteOne({ _id: new ObjectId(id) });
-        res.sendStatus(200);
+        res.sendStatus(204);
     } catch (err) {
         res.status(500).send(err.message);
     }
 });
 
 app.put("/messages/:id", async (req, res) => {
-    const messageSchema = Joi.object({
-        to: Joi.string().custom(value => stripHtml(value)).trim().required(),
-        text: Joi.string().custom(value => stripHtml(value)).trim().required(),
-        type: Joi.string().custom(value => stripHtml(value)).required().valid("private_message", "message")
-    });
-    const { error, value } = messageSchema.validate(req.body, { abortEarly: false });
-    console.log("foi");
+    const from = req.headers.user;
+    const { error, value } = messageSchema.validate({ ...req.body, from }, { abortEarly: false });
 
     if (error) return res.status(422).send(error.details.map(detail => detail.message));
 
@@ -172,9 +165,6 @@ app.put("/messages/:id", async (req, res) => {
     const text = value.text.result;
     const { type } = value;
     const { id } = req.params;
-    const from = req.headers.user;
-
-    console.log("foi tb");
 
     try {
         if (!(await db.collection("participants").findOne({ name: from }))) {
--- src/filterInactiveParticipants.js
+++ src/filterInactiveParticipants.js
@@ -5,25 +5,25 @@ import { MS_STATUS } from "./constants.js";
 export default async function filterInactiveParticipants(db) {
     try {
         const MS_DIFF = Date.now() - MS_STATUS;
-        const participants = await db.collection("participants").find(
+        const inactiveParticipants = await db.collection("participants").find(
             { lastStatus: { $lt: MS_DIFF } }
         ).toArray();
+
+        if (inactiveParticipants.length < 1) return;
 
         await db.collection("participants").deleteMany(
             { lastStatus: { $lt: MS_DIFF } }
         );
 
-        if (participants.length > 0) {
-            await db.collection("messages").insertMany(participants.map((p) => {
-                return {
-                    from: p.name,
-                    to: "Todos",
-                    text: "sai da sala...",
-                    type: "status",
-                    time: dayjs().locale("pt-br").format("HH:mm:ss")
-                };
-            }));
-        }
+        await db.collection("messages").insertMany(inactiveParticipants.map((p) => {
+            return {
+                from: p.name,
+                to: "Todos",
+                text: "sai da sala...",
+                type: "status",
+                time: dayjs().locale("pt-br").format("HH:mm:ss")
+            };
+        }));
     } catch (err) {
         console.log(err.message);
     }
